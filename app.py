fda420b5b5db855219000f408699d4c5113089b9
OmegasDev <omegasantiago52@gmail.com>
2025-02-15 22:43:34 +0100

    Added requirements.txt

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "Social Downloader API is running!"
+    return jsonify({"message": "Social Downloader API is running!"})
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
