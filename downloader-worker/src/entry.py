fb32f15921a751cd845c54edab51a6f447f2aa46
OmegasDev <omegasantiago52@gmail.com>
2025-02-15 11:17:06 +0100

    Initial commit

--- downloader-worker/src/entry.py
+++
@@ -1,6 +0,0 @@
-from pyodide.ffi import to_js
-
-async def fetch(request):
-    body = "Hello! Your Python Cloudflare Worker is running."
-    headers = {"Content-Type": "text/plain"}
-    return to_js((body, {"status": 200, "headers": headers}))
